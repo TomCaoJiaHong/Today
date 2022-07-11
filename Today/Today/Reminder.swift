9a5ea63fc4703bd5aa64b051bca085dabd88fab5
TomCaoJiaHong <1841167049@qq.com>
2022-07-11 14:25:21 -0300

    create models folder and reminder file

---
+++ Today/Today/Reminder.swift
@@ -0,0 +1,14 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 曹嘉鸿 on 2022-07-11.
+//
+
+import Foundation
+
+struct Reminder{
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+}
