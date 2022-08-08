bcc4f669d5457908d6c33f3b36c284eaf6a467f5
TomCaoJiaHong <1841167049@qq.com>
2022-08-08 11:24:37 -0300

    CREATE A reminder model

---
+++ Today/Today/Models/Reminder.swift
@@ -0,0 +1,14 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 曹嘉鸿 on 2022-08-08.
+//
+
+import Foundation
+
+struct Reminder{
+    var title: String
+    var dueDate: Date
+    var notes:String? = nil
+}
